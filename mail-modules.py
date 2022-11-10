ba593ae262883f60891befb5b0f31dc59b4f67e2
cupial <mj.cupial@gmail.com>
2022-11-10 17:24:36 +0100

    #1 run to send email

--- mail-modules.py
+++ mail-modules.py
@@ -8,15 +8,20 @@ def send_email():
     body = """
     This is my first mail sent to you. I'm so excited! :D
     """
-    em = EmailMessage()
-    em['From'] = email_sender
-    em['To'] = email_receiver
-    em['subject'] = subject
-    em.set_content(body)
+    # em = EmailMessage()
+    # em['From'] = email_sender
+    # em['To'] = email_receiver
+    # em['subject'] = subject
+    # em.set_content(body)
+    #
+    # context = ssl.create_default_context()
+    # with smtplib.SMTP_SSL('smtp.mail.yahoo.com', 465, context=context) as smtp:
+    #     smtp.login(email_sender, email_password)
+    #     smtp.sendmail(email_sender, email_receiver, em.as_string())
 
-    context = ssl.create_default_context()
-    with smtplib.SMTP_SSL('smtp.mail.yahoo.com', 465, context=context) as smtp:
-        smtp.login(email_sender, email_password)
-        smtp.sendmail(email_sender, email_receiver, em.as_string())
-
+    server = smtplib.SMTP('smtp.mail.yahoo.com', 587)
+    server.starttls()
+    server.login(email_sender, email_password)
+    server.sendmail(email_sender, email_receiver, subject)
+    print('mail sent')
 send_email()
