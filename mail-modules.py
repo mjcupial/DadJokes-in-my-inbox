80304f0f8d24333bae218c855d9ed08c3373381c
cupial <mj.cupial@gmail.com>
2022-11-10 16:33:35 +0100

    some code added including env variables

--- mail-modules.py
+++ mail-modules.py
@@ -1,1 +1,22 @@
+from email.message import EmailMessage  #https://docs.python.org/3/library/email.html
+import os, ssl, smtplib
 def send_email():
+    email_sender = os.getenv('BOTMAIL')
+    email_password = os.environ.get('BOTPASS')
+    email_receiver = 'mj.cupial@gmail.com'
+    subject = "Welcome form Fifi Bot!"
+    body = """
+    This is my first mail sent to you. I'm so excited! :D
+    """
+    em = EmailMessage()
+    em['From'] = email_sender
+    em['To'] = email_receiver
+    em['subject'] = subject
+    em.set_content(body)
+
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL('smtp.mail.yahoo.com', 465, context=context) as smtp:
+        smtp.login(email_sender, email_password)
+        smtp.sendmail(email_sender, email_receiver, em.as_string())
+
+send_email()
